a7117e5e632426f38083216af70d3639b8da31ca
Rajatm544 <rajatm544@gmail.com>
2020-07-27 22:15:36 +0530

    render the Main component in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
+import Main from "./components/Main";
 
 function App() {
-    return <div className="App">Hello World.</div>;
+    return <Main />;
 }
 
 export default App;
