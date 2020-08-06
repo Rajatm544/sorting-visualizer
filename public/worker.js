fa0c835d37a593e281a581d79c224c83c5e3cfa1
Rajatm544 <rajatm544@gmail.com>
2020-08-06 18:40:12 +0530

    add service worker and make the app a PWA

---
+++ public/worker.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-restricted-globals */
+let cacheName = "v1";
+const urlsToCache = ["/"];
+
+// Install a service worker
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+// Cache and return requests
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) return response;
+
+            return fetch(event.request);
+        })
+    );
+});
+
+// Update a service worker
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== cacheName) return caches.delete(cache);
+                })
+            );
+        })
+    );
+});
