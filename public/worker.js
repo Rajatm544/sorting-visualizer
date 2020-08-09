4ac440369a3c937484eb3bb8eb637b7b1cd15022
Rajatm544 <rajatm544@gmail.com>
2020-08-09 12:15:50 +0530

    update worker registration

--- public/worker.js
+++ public/worker.js
@@ -1,41 +1,39 @@
-/* eslint-disable no-restricted-globals */
-let cacheName = "v2";
+let CACHE_NAME = "v1";
+let urlsToCache = ["/"];
+
 // Install a service worker
 self.addEventListener("install", (event) => {
-    console.log("Service Workers: Installed");
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log("Opened cache");
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
 // Cache and return requests
 self.addEventListener("fetch", (event) => {
     event.respondWith(
-        fetch(event.request)
-            .then((res) => {
-                //Make clone of response
-                const resClone = res.clone();
-                // Open cache
-                caches.open(cacheName).then((cache) => {
-                    // Add response to the cache
-                    cache.put(event.request, resClone);
-                });
-                return res;
-            })
-            .catch((err) =>
-                caches
-                    .match(event.request)
-                    .then((res) => res)
-                    .catch((err) => console.error(err))
-            )
+        caches.match(event.request).then(function (response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
     );
 });
 
 // Update a service worker
 self.addEventListener("activate", (event) => {
+    let cacheWhitelist = ["v1"];
     event.waitUntil(
         caches.keys().then((cacheNames) => {
             return Promise.all(
-                cacheNames.map((cache) => {
-                    if (cache !== cacheName) {
-                        return caches.delete(cache);
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
                     }
                 })
             );
